a51de4dc6d7492ed82d540b5b3b5a210ddea641c
Fakhry Macbook <fakhry.ikhsan.firdaus@gmail.com>
2018-10-05 09:55:40 +0700

    Initial Commit

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,9 +6,37 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  items: any[];
+  
 
   constructor(public navCtrl: NavController) {
+    this.items = [
+      {
+        'name':'Pokémon Yellow',
+        'deskripsi':'kuning',
+        'time':'11.00'
+      },
+      {
+        'name':'Super Metroid',
+        'deskripsi':'hijau', 
+        'time':'12.00'
+      },
+      {
+        'name':'Mega Man X',
+        'deskripsi':'merah',
+        'time':'13.00'
+      },
+      {
+        'name':"super mario",
+        'deskripsi':"orange", 
+        'time':'14.00'
+      }
+    ];
+  }
 
+  itemSelected(item: any) {
+    console.log("Selected Item", item.name);
+    console.log("Selected Item 1", item.deskripsi);
   }
 
 }
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -6,9 +6,31 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  items: any[];
 
   constructor(public navCtrl: NavController) {
-
+    this.items = [
+      {
+        'name':'Pokémon Yellow',
+        'deskripsi':'kuning',
+        'time':'11.00'
+      },
+      {
+        'name':'Super Metroid',
+        'deskripsi':'hijau', 
+        'time':'12.00'
+      },
+      {
+        'name':'Mega Man X',
+        'deskripsi':'merah',
+        'time':'13.00'
+      },
+      {
+        'name':"super mario",
+        'deskripsi':"orange", 
+        'time':'14.00'
+      }
+    ];
   }
 
 }
